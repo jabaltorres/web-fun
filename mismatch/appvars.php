5897d573563ce4758b4e7165b076922ebffbcfbc
Jabal Torres <info@jabaltorres.com>
2019-01-22 19:32:43 -0800

    mismatch relative

---
+++ mismatch/appvars.php
@@ -0,0 +1,7 @@
+<?php
+  // Define application constants
+  define('MM_UPLOADPATH', 'images/');
+  define('MM_MAXFILESIZE', 128000);      // 128 KB
+  define('MM_MAXIMGWIDTH', 1000);        // 120 pixels
+  define('MM_MAXIMGHEIGHT', 1000);       // 120 pixels
+?>
