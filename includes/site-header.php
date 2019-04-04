8cd17aeda458301fe79ec444709983e8359e3fea
Jabal Torres <info@jabaltorres.com>
2019-04-03 17:20:18 -0700

    resolved https error for google fonts

--- includes/site-header.php
+++ includes/site-header.php
@@ -15,8 +15,8 @@
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo $url; ?>/public/images/apple-touch-icon-114x114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $url; ?>/public/images/apple-touch-icon-144x144-precomposed.png">
 
-    <link href='http://fonts.googleapis.com/css?family=Gudea|Old+Standard+TT' rel='stylesheet' type='text/css'>
-    <link href="https://fontastic.s3.amazonaws.com/mWtd8iTgMRgyWPztf4g6Zh/icons.css" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Gudea|Old+Standard+TT" rel="stylesheet" type="text/css">
+    <link href="//fontastic.s3.amazonaws.com/mWtd8iTgMRgyWPztf4g6Zh/icons.css" rel="stylesheet">
     
     <link rel="stylesheet" href="<?php echo $url; ?>/css/slick.css">
     <link rel="stylesheet" href="<?php echo $url; ?>/css/style.css">
