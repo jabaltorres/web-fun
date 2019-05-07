8e2db60acba1300ce956478d028228b80bd2bd91
Jabal Torres <jtorres@adaptiveinsights.com>
2019-05-07 07:43:23 -0700

    Added required input attributes to the form fields.

--- demos/forms/forms.php
+++ demos/forms/forms.php
@@ -19,16 +19,16 @@
 
         <form method="post" action="acknowledge.php">
             <label for="name">Full Name:</label>
-            <input type="text" name="name" id="name">
+            <input type="text" name="name" id="name" required>
 
             <label for="email">Email Address:</label>
-            <input type="email" name="email" id="email">
+            <input type="email" name="email" id="email" required>
 
             <label for="subject">Subject:</label>
-            <input type="text" name="subject" id="subject">
+            <input type="text" name="subject" id="subject" required>
 
             <label for="message">Message:</label>
-            <textarea name="message" id="message"></textarea>
+            <textarea name="message" id="message" required></textarea>
 
             <input type="submit" name="send" value="Send Message" class="btn btn-primary">
         </form>
