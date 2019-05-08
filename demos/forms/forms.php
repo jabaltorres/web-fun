f49437d0691e3a491e23c4b397ee2b237ebc3354
Jabal Torres <jtorres@adaptiveinsights.com>
2019-05-07 21:05:11 -0700

    used bootstrap classes for form inputs. started the validation process.

--- demos/forms/forms.php
+++ demos/forms/forms.php
@@ -1,8 +1,34 @@
 <?php
     require_once '../../private/initialize.php';
+
+    if ( is_post_request() ) {
+        $jtMessage = [];
+        $jtMessage['name'] = $_POST['name'] ?? '';
+        $jtMessage['email'] = $_POST['email'] ?? '';
+        $jtMessage['subject'] = $_POST['subject'] ?? '';
+        $jtMessage['message'] = $_POST['message'] ?? '';
+
+//        echo $jtMessage['name'] . '<br>';
+//        echo $jtMessage['email'] . '<br>';
+//        echo $jtMessage['subject'] . '<br>';
+//        echo $jtMessage['message'] . '<br>';
+
+        // https://php.net/manual/en/function.print-r.php
+//        print_r ($jtMessage);
+
+        echo '<ul>';
+            foreach($jtMessage as $jtMessageItem) {
+                echo "<li>" . h($jtMessageItem) . "</li>";
+            }
+        echo '</ul>';
+    } else {
+
+    }
+
     $title = "Form Page"; // this is for <title>
-    $page_title = "This is the form page"; //this is for breadcrumbs if I want a custom title other than the default
-    $addCSS = ""; //custom CSS for this page only
+    $page_title = "This is the form page";
+    $page_subheading = "Make your own bacon ipsum";
+
     include_once(INCLUDES_PATH . '/site-header.php');
 ?>
 
@@ -12,23 +38,38 @@
 
     <section class="forms">
 
-        <div class="hgroup">
-            <h1>Forms</h1>
-            <p class="lead">This would be a great place to test form validation.</p>
-        </div>
+        <header>
+            <h1 class="h2"><?php echo $page_title; ?></h1>
+            <h2 class="h3"><?php echo $page_subheading; ?></h2>
+        </header>
 
-        <form method="post" action="acknowledge.php">
-            <label for="name">Full Name:</label>
-            <input type="text" name="name" id="name" required>
+        <form method="post" action="<?php echo url_for('../forms/forms.php'); ?>">
 
-            <label for="email">Email Address:</label>
-            <input type="email" name="email" id="email" required>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Full Name:</label>
+                <input type="text" name="name" class="form-control" id="exampleFormControlInput1" placeholder="John Smith" required>
+            </div>
 
-            <label for="subject">Subject:</label>
-            <input type="text" name="subject" id="subject" required>
+            <div class="form-group">
+                <label for="exampleFormControlInput2">Email address:</label>
+                <input type="email" name="email" class="form-control" id="exampleFormControlInput2" placeholder="name@example.com" required>
+            </div>
 
-            <label for="message">Message:</label>
-            <textarea name="message" id="message" required></textarea>
+            <div class="form-group">
+                <label for="exampleFormControlSelect1">Subject</label>
+                <select class="form-control" name="subject" id="exampleFormControlSelect1">
+                    <option>Hello</option>
+                    <option>Compliment</option>
+                    <option>Insult</option>
+                    <option>Inquiry</option>
+                    <option>Sales Pitch</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Message:</label>
+                <textarea class="form-control" name="message" id="exampleFormControlTextarea1" rows="3" required></textarea>
+            </div>
 
             <input type="submit" name="send" value="Send Message" class="btn btn-primary">
         </form>
