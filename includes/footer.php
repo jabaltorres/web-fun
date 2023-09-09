63d6041ed6446b76bca8ff794a480c1d8b0129d7
FiveTwoFive <jabal@fivetwofive.com>
2023-09-09 15:41:55 -0700

    removed commented out jquery scripts

--- includes/footer.php
+++ includes/footer.php
@@ -14,9 +14,7 @@
         <li><a href="https://github.com/capitalJT/web-fun/wiki" target="_blank">Git Wiki</a></li>
       </ul>
     </footer>
-    
-    <!-- <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script> -->
-    <!-- <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script> -->
+
     <!-- <script src="dist/scripts/scripts.min.js"></script> -->
     <script src="<?php echo $url; ?>/dist/scripts/scripts.js"></script>
 
