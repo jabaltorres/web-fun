c618ed3b26bbdf0d2b62c74c27c328d47d9f2f13
FiveTwoFive <jabal@fivetwofive.com>
2023-09-10 06:38:56 -0700

    relocated function

--- demos/js-fun/index.php
+++ demos/js-fun/index.php
@@ -7,7 +7,7 @@
     $page_heading = "Javascript playground";
 
     // This is the subheading
-    $page_subheading = "Make your own scripts";
+    $page_subheading = "Scripting Sandbox";
 
     //custom CSS for this page only
 	$custom_class = "js-fun-page mb-4";
@@ -28,22 +28,11 @@
         </section>
 
         <section class="js-playground">
-            <div class="greeting-msg">
-                <?php
-                    // echo the time of day
-                    $time = date("H");
-                    if ($time < "12") {
-                        echo "Good morning";
-                    } elseif ($time >= "12" && $time < "17") {
-                        echo "Good afternoon";
-                    } elseif ($time >= "17" && $time < "19") {
-                        echo "Good evening";
-                    } elseif ($time >= "19") {
-                        echo "Good night";
-                    }
-                ?>
-            </div>
+
+            <?php timeOfDayGreeting(); ?>
+
             <div class="entry-content">
+                This is rendered content.
                 <?php
                     /**
                      * This is just a comment to remind me that content gets rendered here
@@ -57,19 +46,10 @@
             <h3>This is from the getJSON function in apps.js</h3>
         </section>
 
-		<?php //include(BLOCKS_PATH . '/github-gist.php'); ?>
+        <section>
+            <?php include(BLOCKS_PATH . '/github-gist.php'); ?>
+        </section>
+
     </div>
 
 <?php include_once(INCLUDES_PATH . '/site-footer.php');?>
-
-<script>
-	// this is an IIFE
-    (function () {
-	    // define variables
-	    let name = "John Doe";
-	    let age = 25;
-		console.log( name + " is " + age + " years old." );
-	    console.log('This is from the js-fun.php page');
-    })();
-</script>
-
