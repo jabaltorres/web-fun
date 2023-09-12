770c6888dd9f396115469a6650c80ef33800f1d1
FiveTwoFive <jabal@fivetwofive.com>
2023-09-12 11:36:46 -0700

    minor clean up and tweaks to the hostInfo.js file

--- js/hostInfo.js
+++ js/hostInfo.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
 	if ( $(".host-page").length ) {
 
-		console.log('You are on the host-info.php page');
-
 		let pathname = window.location.pathname;
 		let location = window.location;
 		let host = window.location.host;
 		let origin = window.location.origin;
+		let protocol = window.location.protocol;
 
 		// checks if the pathname is equal to /demos/host-info.php
 		if (pathname === "/demos/host-info.php"){
@@ -14,16 +13,26 @@ $(document).ready(function () {
 			console.log('the location: ', location);
 			console.log('the host: ', host);
 			console.log('the origin: ', origin);
+			console.log('the protocol: ', protocol);
 		} else {
 			console.log('You are NOT on the host-info.php page');
 		}
 
 		// This is displayed on the /demos/host-info.php page
-		if ($("#host-info").length){
-			$("<div>Pathnames: " + pathname + "</div>").appendTo("#host-info");
-			$("<div>Location: " + location + "</div>").appendTo("#host-info");
-			$("<div>Host: " + host + "</div>").appendTo("#host-info");
-			$("<div>Origin: " + origin + "</div>").appendTo("#host-info");
-		}
+		$("<div>Pathnames: " + pathname + "</div>").appendTo("#host-info");
+		$("<div>Location: " + location + "</div>").appendTo("#host-info");
+		$("<div>Host: " + host + "</div>").appendTo("#host-info");
+		$("<div>Origin: " + origin + "</div>").appendTo("#host-info");
 	}
 });
+
+
+(function () {
+	let sect = document.getElementsByClassName("host-page-section");
+	for (let i = 0; i < sect.length; i++) {
+		sect[i].classList.add("border", "border-primary", "rounded", "p-4", "mb-4");
+
+	}
+	console.log("Host Info: " + window.location.host);
+
+})();
