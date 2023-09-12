284e476c50ad2ee773fecc976ffbd495160726a1
FiveTwoFive <jabal@fivetwofive.com>
2023-09-12 11:25:16 -0700

    minor refactor to block headline file

--- incl/block-headline.php
+++ incl/block-headline.php
@@ -1,4 +1,4 @@
 <div class="hgroup">
-  <h1 class="block-headline h3"><?php echo $block_headline; ?></h1>
-  <h2 class="block-subheading h4"><?php echo $block_subheading; ?></h2>
+    <h1 class="block-headline h3"><?php echo $block_headline; ?></h1>
+    <h2 class="block-subheading h5"><?php echo $block_subheading; ?></h2>
 </div>
