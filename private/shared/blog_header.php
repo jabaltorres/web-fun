1f2bc1acc76b089d45277b86e8ac08bbe7064037
FiveTwoFive <jabal@fivetwofive.com>
2024-05-03 13:00:05 -0700

    Added blog header and footer

---
+++ private/shared/blog_header.php
@@ -0,0 +1,15 @@
+<style>
+    .container {
+        margin-top: 20px;
+        max-width: 800px;
+        border: solid thin #ccc;
+        padding: 20px;
+    }
+</style>
+
+<header>
+    <h1>Web Fun</h1>
+    <a href="/public/blog/">Blog</a>
+</header>
+
+<div class="container">
