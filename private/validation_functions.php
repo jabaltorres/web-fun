788bd39d68a37330a493a7f2cc286bcb71a96b24
FiveTwoFive <jabal@fivetwofive.com>
2024-05-04 08:46:33 -0700

    Added has_unique_username function

--- private/validation_functions.php
+++ private/validation_functions.php
@@ -112,4 +112,23 @@
     return $page_count === 0;
   }
 
+  // has_unique_username('johnqpublic')
+  // * Validates uniqueness of admins.username
+  // * For new records, provide only the username.
+  // * For existing records, provide current ID as second argument
+  //   has_unique_username('johnqpublic', 4)
+  function has_unique_username($username, $current_id="0") {
+    global $db;
+
+    $sql = "SELECT * FROM admins ";
+    $sql .= "WHERE username='" . db_escape($db, $username) . "' ";
+    $sql .= "AND id != '" . db_escape($db, $current_id) . "'";
+
+    $result = mysqli_query($db, $sql);
+    $admin_count = mysqli_num_rows($result);
+    mysqli_free_result($result);
+
+    return $admin_count === 0;
+  }
+
 ?>
