c34772c1fba4c7bc2459ef7102d33f2bf9b7abf2
FiveTwoFive <jabal@fivetwofive.com>
2024-05-04 09:30:21 -0700

    Added temp blog post pages for experimentation. The goad is to have a local place to capture and publish notes.

---
+++ public/blog/posts/2023-09-09-post-3.php
@@ -0,0 +1,11 @@
+<?php
+    require_once('../../../private/initialize.php');
+    include(SHARED_PATH . '/blog_header.php');
+
+    echo "<h1>Post 3</h1>";
+?>
+
+This is some content for post 3.
+
+
+<?php include(SHARED_PATH . '/blog_footer.php'); ?>
---
+++ public/blog/posts/2024-05-02-post-4.php
@@ -0,0 +1,12 @@
+<?php
+    require_once('../../../private/initialize.php');
+    include(SHARED_PATH . '/blog_header.php');
+?>
+
+<h1>Welcome back! Long time now see.</h1>
+
+<p>Coming back to this. I'll just start with a brain dump. Time to start getting back into the habit of actually using brain. I feel so out of practice with every thing. I need to cut through the mental fog.</p>
+
+
+
+<?php include(SHARED_PATH . '/blog_footer.php'); ?>
