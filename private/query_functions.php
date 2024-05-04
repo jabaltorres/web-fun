463d46e0a1f805a87d51f0b423042729647bdc97
FiveTwoFive <jabal@fivetwofive.com>
2024-05-03 23:16:49 -0700

    minor code cleanup

--- private/query_functions.php
+++ private/query_functions.php
@@ -327,18 +327,18 @@
         return $result;
     }
 
-function find_contact_by_id($id) {
-    global $db;
-    $sql = "SELECT contact_list.*, rankings.rank_description FROM contact_list ";
-    $sql .= "LEFT JOIN rankings ON contact_list.rank_id = rankings.rank_id ";
-    $sql .= "WHERE contact_list.id='" . db_escape($db, $id) . "' ";
-    $sql .= "LIMIT 1";
-    $result = mysqli_query($db, $sql);
-    confirm_result_set($result);
-    $contact = mysqli_fetch_assoc($result);
-    mysqli_free_result($result);
-    return $contact;
-}
+    function find_contact_by_id($id) {
+        global $db;
+        $sql = "SELECT contact_list.*, rankings.rank_description FROM contact_list ";
+        $sql .= "LEFT JOIN rankings ON contact_list.rank_id = rankings.rank_id ";
+        $sql .= "WHERE contact_list.id='" . db_escape($db, $id) . "' ";
+        $sql .= "LIMIT 1";
+        $result = mysqli_query($db, $sql);
+        confirm_result_set($result);
+        $contact = mysqli_fetch_assoc($result);
+        mysqli_free_result($result);
+        return $contact;
+    }
 
     function validate_contact($contact) {
         $errors = [];
--- public/contacts/index.php
+++ public/contacts/index.php
@@ -1,72 +1,60 @@
 <?php
-    require_once($_SERVER['DOCUMENT_ROOT'] . '/private/initialize.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/private/initialize.php');
+require_login();
 
-    require_login();
+$title = "DB Test Page";
+$page_heading = "This is the DB Test page";
+$page_subheading = "Welcome to the DB test page";
+$custom_class = "db-test-page";
+$contact_set = find_all_contacts();
 
-    $title = "DB Test Page";
-    // this is for <title>
-
-    $page_heading = "This is the DB Test page";
-    // This is for breadcrumbs if I want a custom title other than the default
-
-    $page_subheading = "Welcome to the DB test page";
-    // This is the subheading
-
-    $custom_class = "db-test-page";
-    //custom CSS for this page only
-
-    $contact_set = find_all_contacts();
-    // From globe_bank tutorial
-
-    include_once(INCLUDES_PATH . '/site-header.php');
+include_once(INCLUDES_PATH . '/site-header.php');
 ?>
 
 <div class="container <?php echo $custom_class; ?>">
-
     <?php
         include_once(INCLUDES_PATH . '/masthead.php');
         include_once(INCLUDES_PATH . '/navigation.php');
     ?>
 
     <section>
-        <?php include_once(INCLUDES_PATH . '/headline-page.php');?>
-        <?php include_once(INCLUDES_PATH . '/db-menu.php');?>
+        <?php
+            include_once(INCLUDES_PATH . '/headline-page.php');
+            include_once(INCLUDES_PATH . '/db-menu.php');
+        ?>
     </section>
 
     <section>
-        <div>
-            <h4 class="mb-2 h4 font-weight-bold">Contact Entries</h4>
-            <p>This uses the `find_all_contacts()` function from the `query_functions.php` file.</p>
-        </div>
-
+        <h4 class="mb-2 h4 font-weight-bold">Contact Entries</h4>
+        <p>This uses the `find_all_contacts()` function from the `query_functions.php` file.</p>
         <table class="table table-striped border">
             <thead class="thead-dark">
-                <tr>
-                    <th scope="col" class="font-weight-bold">ID</th>
-                    <th scope="col" class="font-weight-bold">First Name</th>
-                    <th scope="col" class="font-weight-bold">Last Name</th>
-                    <th scope="col" class="font-weight-bold">Email</th>
-                    <th scope="col">&nbsp;</th>
-                    <th scope="col">&nbsp;</th>
-                    <th scope="col">&nbsp;</th>
-                </tr>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">&nbsp;</th>
+                <th scope="col">&nbsp;</th>
+                <th scope="col">&nbsp;</th>
+            </tr>
             </thead>
             <tbody>
-                <?php while($contact = mysqli_fetch_assoc($contact_set)): ?>
-                    <tr class="">
-                        <td class="align-middle"><?php echo h($contact['id']); ?></td>
-                        <td class="align-middle"><?php echo h($contact['first_name']); ?></td>
-                        <td class="align-middle"><?php echo h($contact['last_name']); ?></td>
-                        <td class="align-middle"><?php echo h($contact['email']); ?></td>
-                        <td><a class="action btn btn-sm btn-info d-block mx-auto" href="<?php echo url_for('/contacts/contact-show.php?id=' . h(u($contact['id']))); ?>">View</a></td>
-                        <td><a class="action btn btn-sm btn-warning d-block mx-auto" href="<?php echo url_for('/contacts/contact-edit.php?id=' . h(u($contact['id']))); ?>">Edit</a></td>
-                        <td><a class="action btn btn-sm btn-danger d-block mx-auto" href="<?php echo url_for('/contacts/contact-delete.php?id=' . h(u($contact['id']))); ?>">Delete</a></td>
-                    </tr>
-                <?php endwhile; ?>
+            <?php while ($contact = mysqli_fetch_assoc($contact_set)) : ?>
+                <tr>
+                    <td><?php echo h($contact['id']); ?></td>
+                    <td><?php echo h($contact['first_name']); ?></td>
+                    <td><?php echo h($contact['last_name']); ?></td>
+                    <td><?php echo h($contact['email']); ?></td>
+                    <td><a class="action btn d-block mx-auto btn-info" href="<?php echo url_for('/contacts/contact-show.php?id=' . h(u($contact['id']))); ?>">View</a></td>
+                    <td><a class="action btn d-block mx-auto btn-warning" href="<?php echo url_for('/contacts/contact-edit.php?id=' . h(u($contact['id']))); ?>">Edit</a></td>
+                    <td><a class="action btn d-block mx-auto btn-danger" href="<?php echo url_for('/contacts/contact-delete.php?id=' . h(u($contact['id']))); ?>">Delete</a></td>
+                </tr>
+            <?php endwhile; ?>
+            <?php mysqli_free_result($contact_set); ?>
             </tbody>
         </table>
-        <?php mysqli_free_result($contact_set); ?>
     </section>
-
 </div><!-- end .container -->
+
 <?php include_once(INCLUDES_PATH . '/site-footer.php'); ?>
