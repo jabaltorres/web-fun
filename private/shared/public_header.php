aadb59bb59f8f569472473cef19c4797684a278d
FiveTwoFive <jabal@fivetwofive.com>
2024-05-08 20:36:00 -0700

    Added bootstrap navbar to public_header

--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -1,5 +1,7 @@
 <?php
-    $url = $url ?? '';
+if(!isset($page_title)) { $page_title = 'Users Area'; }
+$url = $url ?? '';
+$is_logged_in = isset($_SESSION['user_id']); // Check if user is logged in
 ?>
 <!doctype html>
 
@@ -17,25 +19,34 @@
 
 <body>
 
-    <header>
-        <h1>
-            <a href="<?php echo url_for('/index.php'); ?>">
-                <img class="logo" src="<?php echo url_for('/images/gbi_logo.png'); ?>" alt="logo"/>
-            </a>
-        </h1>
-    </header>
-    <div class="webfun-link">
-        <nav>
-            <ul>
-                <li>
-                    <a href="/">Back to WebFun</a>
-                </li>
-                <li>
-                    <a href="staff/">Staff Area</a>
-                </li>
-                <li>
-                    <a href="staff/admins/">Admin Area</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="/public/">KrateCMS</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/public/contacts/">Contacts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/public/users/">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/public/staff/admins/">Admins</a>
+                    </li>
+                </ul>
+                <!-- Update navbar text to reflect user status -->
+                <span class="navbar-text">
+                    <?php if ($is_logged_in): ?>
+                        Welcome back, <?= htmlspecialchars($_SESSION['first_name']); ?>! <a href="/public/users/logout.php">Log Out</a>
+                    <?php else: ?>
+                        Welcome Guest! Please <a href="/public/users/login.php">Log In</a> or <a href="/public/users/register.php">Register</a>.
+                    <?php endif; ?>
+                </span>
+            </div>
+        </div>
+    </nav>
+
+    <?php echo display_session_message(); ?>
