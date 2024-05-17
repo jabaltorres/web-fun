a60a9de48c3a453bd9210e3c5e6348c35e807240
jaballer <jtorres@activecampaign.com>
2024-05-16 21:58:35 -0700

    Simple reformatted code.

--- public/index.php
+++ public/index.php
@@ -1,49 +1,44 @@
 <?php
+// Require initialization file
+require_once($_SERVER['DOCUMENT_ROOT'] . '/../private/initialize.php');
 
-    // Require initialization file
-    require_once($_SERVER['DOCUMENT_ROOT'] . '/../private/initialize.php');
+// Check if user is logged in
+$loggedIn = isset($_SESSION['user_id']);
 
-    // Check if user is logged in
-    $loggedIn = isset($_SESSION['user_id']);
-
-?>
-
-<?php
 
 $preview = false;
-if(isset($_GET['preview'])) {
-  // previewing should require admin to be logged in
-  $preview = $_GET['preview'] == 'true' ? true : false;
+if (isset($_GET['preview'])) {
+    // previewing should require admin to be logged in
+    $preview = $_GET['preview'] == 'true' ? true : false;
 }
 $visible = !$preview;
 
-if(isset($_GET['id'])) {
-  $page_id = $_GET['id'];
-  $page = find_page_by_id($page_id, ['visible' => $visible]);
-  if(!$page) {
-    redirect_to(url_for('/index.php'));
-  }
-  $subject_id = $page['subject_id'];
-  $subject = find_subject_by_id($subject_id, ['visible' => $visible]);
-  if(!$subject) {
-    redirect_to(url_for('/index.php'));
-  }
-
-} elseif(isset($_GET['subject_id'])) {
-  $subject_id = $_GET['subject_id'];
-  $subject = find_subject_by_id($subject_id, ['visible' => $visible]);
-  if(!$subject) {
-    redirect_to(url_for('/index.php'));
-  }
-  $page_set = find_pages_by_subject_id($subject_id, ['visible' => $visible]);
-  $page = mysqli_fetch_assoc($page_set); // first page
-  mysqli_free_result($page_set);
-  if(!$page) {
-    redirect_to(url_for('/index.php'));
-  }
-  $page_id = $page['id'];
+if (isset($_GET['id'])) {
+    $page_id = $_GET['id'];
+    $page = find_page_by_id($page_id, ['visible' => $visible]);
+    if (!$page) {
+        redirect_to(url_for('/index.php'));
+    }
+    $subject_id = $page['subject_id'];
+    $subject = find_subject_by_id($subject_id, ['visible' => $visible]);
+    if (!$subject) {
+        redirect_to(url_for('/index.php'));
+    }
+} elseif (isset($_GET['subject_id'])) {
+    $subject_id = $_GET['subject_id'];
+    $subject = find_subject_by_id($subject_id, ['visible' => $visible]);
+    if (!$subject) {
+        redirect_to(url_for('/index.php'));
+    }
+    $page_set = find_pages_by_subject_id($subject_id, ['visible' => $visible]);
+    $page = mysqli_fetch_assoc($page_set); // first page
+    mysqli_free_result($page_set);
+    if (!$page) {
+        redirect_to(url_for('/index.php'));
+    }
+    $page_id = $page['id'];
 } else {
-  // nothing selected; show the homepage
+    // nothing selected; show the homepage
 }
 
 ?>
@@ -51,34 +46,35 @@ if(isset($_GET['id'])) {
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
 <div id="main" class="homepage">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3">
-                <?php include(SHARED_PATH . '/public_navigation.php'); ?>
-            </div>
-            <div class="col-md-9">
-                <section class="user-content">
-                    <?php if ($loggedIn): ?>
-                        <p class="mb-0">Welcome, <?= htmlspecialchars($_SESSION['first_name']); ?>! Here is the exclusive content for logged-in users.</p>
-                    <?php else: ?>
-                        <p class="mb-0">Please <a href="users/login.php">log in</a> to view this section.</p>
-                    <?php endif; ?>
-                </section>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+          <?php include(SHARED_PATH . '/public_navigation.php'); ?>
+      </div>
+      <div class="col-md-9">
+        <section class="user-content">
+            <?php if ($loggedIn): ?>
+              <p class="mb-0">Welcome, <?= htmlspecialchars($_SESSION['first_name']); ?>! Here is the exclusive content
+                for logged-in users.</p>
+            <?php else: ?>
+              <p class="mb-0">Please <a href="users/login.php">log in</a> to view this section.</p>
+            <?php endif; ?>
+        </section>
 
-                <div class="page">
-                    <?php
-                        if(isset($page)) {
-                            // show the page from the database
-                            $allowed_tags = '<div><img><h1><h2><p><br><strong><em><ul><li>';
-                            echo strip_tags($page['content'], $allowed_tags);
-                        } else {
-                            include(SHARED_PATH . '/static_homepage.php');
-                        }
-                    ?>
-                </div>
-            </div>
+        <div class="page">
+            <?php
+            if (isset($page)) {
+                // show the page from the database
+                $allowed_tags = '<div><img><h1><h2><p><br><strong><em><ul><li>';
+                echo strip_tags($page['content'], $allowed_tags);
+            } else {
+                include(SHARED_PATH . '/static_homepage.php');
+            }
+            ?>
         </div>
+      </div>
     </div>
+  </div>
 </div>
 
 <?php include(SHARED_PATH . '/public_footer.php'); ?>
