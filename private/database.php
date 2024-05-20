9afa33d132ec004c1b265e3de845594996749eb9
jaballer <jtorres@activecampaign.com>
2024-05-20 06:23:38 -0700

    Revert "Refactor database connection handling with improved error management"
    This reverts commit 7c8ff8b3f6a624d8e545d080df9ea73dc294a1e5.

--- private/database.php
+++ private/database.php
@@ -1,41 +1,36 @@
 <?php
 
-require_once('db_credentials.php');
+    require_once('db_credentials.php');
 
-function db_connect()
-{
-    try {
-        $connection = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-        if ($connection->connect_error) {
-            throw new Exception("Database connection failed: " . $connection->connect_error);
-        }
+    function db_connect() {
+        $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+        confirm_db_connect();
         return $connection;
-    } catch (Exception $e) {
-        // Optionally log $e->getMessage() to a log file
-        exit('Database connection failed. Please try again later.');
     }
-}
 
-function db_disconnect($connection)
-{
-    if (isset($connection)) {
-        $connection->close();
+    function db_disconnect($connection) {
+        if(isset($connection)) {
+            mysqli_close($connection);
+        }
     }
-}
 
-function db_escape($connection, $string)
-{
-    return $connection->real_escape_string($string);
-}
-
-// This function is no longer needed if exceptions are used in db_connect()
-// function confirm_db_connect() { }
-
-function confirm_result_set($result_set)
-{
-    if (!$result_set) {
-        exit("Database query failed.");
+    function db_escape($connection, $string) {
+        return mysqli_real_escape_string($connection, $string);
     }
-}
+
+    function confirm_db_connect() {
+        if(mysqli_connect_errno()) {
+            $msg = "Database connection failed: ";
+            $msg .= mysqli_connect_error();
+            $msg .= " (" . mysqli_connect_errno() . ")";
+            exit($msg);
+        }
+    }
+
+    function confirm_result_set($result_set) {
+        if (!$result_set) {
+            exit("Database query failed.");
+        }
+    }
 
 ?>
