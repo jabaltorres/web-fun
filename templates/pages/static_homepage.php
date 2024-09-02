f2ed989452003be03dd9cfb621265ced04e9cbcf
Jabal Torres <jabaltorres@gmail.com>
2024-09-02 12:58:00 -0700

    Updated links on homepage

--- templates/pages/static_homepage.php
+++ templates/pages/static_homepage.php
@@ -11,21 +11,21 @@
             <img class="mb-3" src="<?php echo IMAGES_PATH; ?>/homepage_assets/family_buying_home_L30707.jpg" width="250" height="166" alt="Family in front of sold home"/>
             <h3>Contacts</h3>
             <p>Bacon ipsum dolor amet turducken jowl flank strip steak pork shank.</p>
-            <a href="/public/contacts" class="btn btn-primary">Contacts</a>
+            <a href="/contacts" class="btn btn-primary">Contacts</a>
         </div>
 
         <div class="col-4">
             <img class="mb-3" src="<?php echo IMAGES_PATH; ?>/homepage_assets/father_daughter_finances_L30717.jpg" width="250" height="166" alt="Father and daughter discussing finances"/>
             <h3>Users</h3>
             <p>Bacon ipsum dolor amet turducken jowl flank strip steak pork shank.</p>
-            <a href="/public/users" class="btn btn-primary">Users</a>
+            <a href="/users" class="btn btn-primary">Users</a>
         </div>
 
         <div class="col-4">
             <img class="mb-3" src="<?php echo IMAGES_PATH; ?>/homepage_assets/owner_salon_L30714.jpg" width="250" height="166" alt="Small business owner"/>
             <h3>Admins</h3>
             <p>Bacon ipsum dolor amet turducken jowl flank strip steak pork shank.</p>
-            <a href="/public/staff/admins" class="btn btn-primary">Admins</a>
+            <a href="/staff/admins" class="btn btn-primary">Admins</a>
         </div>
     </div>
 </div>
