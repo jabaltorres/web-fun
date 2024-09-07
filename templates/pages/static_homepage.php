18aed31eae7f2cfd398103c4d57680496ec272da
Jabal Torres <jabaltorres@gmail.com>
2024-09-06 23:18:38 -0700

    Updated static homepage

--- templates/pages/static_homepage.php
+++ templates/pages/static_homepage.php
@@ -9,24 +9,18 @@
 <div id="content" class="content">
   <div class="row">
     <div class="col-4">
-      <img class="mb-3" src="<?php echo IMAGES_PATH; ?>/homepage_assets/family_buying_home_L30707.jpg" width="250"
-           height="166" alt="Family in front of sold home"/>
-      <h3>Contacts</h3>
+      <h3>Blog</h3>
       <p>Bacon ipsum dolor amet turducken jowl flank strip steak pork shank.</p>
-      <a href="/contacts" class="btn btn-primary">Contacts</a>
+      <a href="/blog/" class="btn btn-primary">Blog</a>
     </div>
 
     <div class="col-4">
-      <img class="mb-3" src="<?php echo IMAGES_PATH; ?>/homepage_assets/father_daughter_finances_L30717.jpg" width="250"
-           height="166" alt="Father and daughter discussing finances"/>
-      <h3>Users</h3>
+      <h3>Vinyl Records</h3>
       <p>Bacon ipsum dolor amet turducken jowl flank strip steak pork shank.</p>
-      <a href="/users" class="btn btn-primary">Users</a>
+      <a href="/records/" class="btn btn-primary">Records</a>
     </div>
 
     <div class="col-4">
-      <img class="mb-3" src="<?php echo IMAGES_PATH; ?>/homepage_assets/owner_salon_L30714.jpg" width="250" height="166"
-           alt="Small business owner"/>
       <h3>Demos</h3>
       <p>Bacon ipsum dolor amet turducken jowl flank strip steak pork shank.</p>
       <a href="/demos/" class="btn btn-primary">Demos</a>
