a4fd8b6bc5303a0e02b78593e8d8aaaedbf977e6
Jabal Torres <jabaltorres@gmail.com>
2024-09-14 09:51:27 -0700

    Udpated footer links

--- templates/layout/footer.php
+++ templates/layout/footer.php
@@ -1,10 +1,11 @@
 <footer class="site-footer">
-    <div class="container">
+    <div class="container text-center">
         <span>&copy; <?php echo date("Y") . "&nbsp;" . $site_owner; ?></span>
         <ul class="d-inline-block">
-            <li><a href="https://github.com/jabaltorres/web-fun" target="_blank">Git Repo</a></li>
-            <li><a href="https://github.com/jabaltorres/web-fun/issues" target="_blank">Git Issues</a></li>
-            <li><a href="https://github.com/jabaltorres/web-fun/wiki" target="_blank">Git Wiki</a></li>
+            <li><a href="https://github.com/jabaltorres/web-fun" target="_blank">Github Repo</a></li>
+            <li><a href="https://github.com/jabaltorres/web-fun/issues" target="_blank">Github Issues</a></li>
+            <li><a href="https://github.com/jabaltorres/web-fun/wiki" target="_blank">Github Wiki</a></li>
+            <li><a href="https://github.com/users/jabaltorres/projects/2" target="_blank">Github Project</a></li>
         </ul>
     </div>
 </footer>
