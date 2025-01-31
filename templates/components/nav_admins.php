72e7cbbc3b098503881e0d1f7eb5f0a0c06cb01a
jaballer <jtorres@activecampaign.com>
2025-01-31 13:58:37 -0800

    Rewrote admin nav as an array of items

--- templates/components/nav_admins.php
+++ templates/components/nav_admins.php
@@ -3,15 +3,25 @@
         <div class="row">
             <div class="col-12">
                 <ul class="nav justify-content-center">
-                    <li class="nav-item"><a class="nav-link text-white" href="/staff/admins/">Admins</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="/staff/admins/new.php">Create New Admin</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="/users/">Users</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="/users/user-add.php">User Add</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="/staff/index.php">Staff</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="/staff/pages/index.php">Pages</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="/staff/subjects/index.php">Subjects</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="/contacts/index.php">Contacts</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="/demos/index.php">Demos</a></li>
+                    <?php
+                    // Define navigation items with ids
+                    $navItems = [
+                        ['label' => 'Admins', 'url' => '/staff/admins/', 'id' => 'admins'],
+                        ['label' => 'Create New Admin', 'url' => '/staff/admins/new.php', 'id' => 'new-admin'],
+                        ['label' => 'Users', 'url' => '/users/', 'id' => 'users'],
+                        ['label' => 'User Add', 'url' => '/users/user-add.php', 'id' => 'user-add'],
+                        ['label' => 'Staff', 'url' => '/staff/index.php', 'id' => 'staff'],
+                        ['label' => 'Pages', 'url' => '/staff/pages/index.php', 'id' => 'pages'],
+                        ['label' => 'Subjects', 'url' => '/staff/subjects/index.php', 'id' => 'subjects'],
+                        ['label' => 'Contacts', 'url' => '/contacts/index.php', 'id' => 'contacts'],
+                        ['label' => 'Demos', 'url' => '/demos/index.php', 'id' => 'demos'],
+                    ];
+
+                    // Generate navigation items
+                    foreach ($navItems as $item) {
+                        echo '<li class="nav-item"><a id="nav-item-' . htmlspecialchars($item['id']) . '" class="text-white nav-link" href="' . htmlspecialchars($item['url']) . '">' . htmlspecialchars($item['label']) . '</a></li>';
+                    }
+                    ?>
                 </ul>
             </div>
         </div>
