219103c70ea4de0cce3897c5741ee128409d27b0
jaballer <jtorres@activecampaign.com>
2025-02-15 09:01:43 -0800

    Removed unused files

--- templates/components/nav_users.php
+++
@@ -1,4 +0,0 @@
-<ul class="nav mb-4">
-    <li class="nav-item"><a class="nav-link" href="/contacts/">Contacts</a></li>
-    <li class="nav-item"><a class="nav-link" href="/staff/">Staff</a></li>
-</ul>
